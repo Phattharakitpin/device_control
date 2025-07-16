ceb93f6648f07df9f3c6a20356a9630885649c3e
Phattharakitpin <pinkaewphattharakit@gmail.com>
2025-07-16 11:40:27 +0700

    Add files via upload

---
+++ ex2/ex2.ino
@@ -0,0 +1,30 @@
+//EX2
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start");
+}
+
+void loop() {
+  for(int greenRoad=1; greenRoad<=4; greenRoad++) {
+    Road(greenRoad);
+    delay(3000);  // รอระหว่างรอบเพื่อให้ดูผลชัดเจน
+  }
+
+  Serial.println("&");
+  Serial.println("Loop back to start");
+  
+  delay(3000);  // รอก่อนเริ่ม loop ใหม่
+}
+
+//Road Circuit
+void Road(int greenRoad){
+  for(int road=1; road<=4; road++){
+    Serial.println(road);
+    if(road == greenRoad){
+      Serial.println("is Green");
+    } else {
+      Serial.println("is Red");
+    }
+  }
+}
